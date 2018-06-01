2ff79e9e7d00764d90c14bdd72b5a4f34b0cc76f
Dendi Handian <dendihandian@gmail.com>
2018-06-02 04:01:22 +0700

    add adoptable remote method endpoint

--- common/models/cat.js
+++ common/models/cat.js
@@ -2,4 +2,20 @@
 
 module.exports = function(Cat) {
 
+  Cat.adoptable = function(id, callback) {
+    Cat.findById(id, function(error, cat) {
+      if (error) return callback("Error", null);
+      if (!cat) return callback("Cat not found", null);
+      let canAdopt = false;
+      if (cat.breed != 'tiger' || (cat.age >= 10))
+      canAdopt = true;
+      callback(null, canAdopt);
+    });
+  }
+
+  Cat.remoteMethod('adoptable', {
+    accepts: {arg: 'id', type: 'any'},
+    returns: {arg: 'adoptable', type: 'boolean'}
+  });
+
 };
